ef0d66f06ebc0e72c5371413e9450888945520ad
ajayvishnu2001 <110541284+ajayvishnu2001@users.noreply.github.com>
2023-03-01 13:26:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
-#from report import pand_rep
+from report import pand_rep
 from forecast import pycaret
-#from mongodb2 import mongo2
+from mongodb2 import mongo2
 
 app = Flask(__name__)
 CORS(app)
 
-#app.register_blueprint(pand_rep)
+app.register_blueprint(pand_rep)
 app.register_blueprint(pycaret)
-#app.register_blueprint(mongo2)
+app.register_blueprint(mongo2)
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0')
